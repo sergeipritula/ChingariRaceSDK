c29c51821f1d418567bac866f66cf3f70e67518b
sergey.pritula <sergey.pritula@onix-systems.com>
2025-03-20 14:01:24 +0200

    removed dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ChingariRaceSdk",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v14), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,37 +13,40 @@ let package = Package(
             targets: ["ChingariRaceSdk"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.0.0-rc.2")),
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxOptional", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/slackhq/PanModal.git", .upToNextMinor(from: "1.2.7")),
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.0"),
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.1.1")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
+//        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.0.0-rc.2")),
+//        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
+//        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+//        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
+//        .package(url: "https://github.com/RxSwiftCommunity/RxOptional", .upToNextMajor(from: "5.0.0")),
+//        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+//        .package(url: "https://github.com/slackhq/PanModal.git", .upToNextMinor(from: "1.2.7")),
+//        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.0"),
+//        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.1.1")),
+//        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ChingariRaceSdk",
-            dependencies: ["Alamofire",
-                           "Moya",
-                           "RxSwift",
-                           "RxDataSources",
-                           "RxOptional",
-                           "SnapKit",
-                           "PanModal",
-                           "SDWebImage",
-                           .product(name: "Lottie", package: "lottie-spm"),
-                           .product(name: "SocketIO", package: "socket.io-client-swift")],
-            resources: [.process("Resources/Country/Media.xcassets"),
-                        .process("Resources/Assets.xcassets")]
-
-
+//            dependencies: ["Alamofire",
+//                           "Moya",
+//                           "RxSwift",
+//                           "RxDataSources",
+//                           "RxOptional",
+//                           "SnapKit",
+//                           "PanModal",
+//                           "SDWebImage",
+//                           .product(name: "Lottie", package: "lottie-spm"),
+//                           .product(name: "SocketIO", package: "socket.io-client-swift")],
+            resources: [
+                .process("Resources/Fonts"),
+                .process("Resources/Sounds"),
+                .process("Resources/Lottie"),
+                .process("Resources/Country/Media.xcassets"),
+                .process("Resources/Country/countries.plist"),
+                .process("Resources/Assets.xcassets")]
         ),
-
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
